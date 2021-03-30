aa9f543266e8f42bcc20c05481c79178172f4224
Weixin Zhou <zwx@rock-chips.com>
2021-03-30 10:51:21 +0800

    backlight: pwm_bl: do not suspend in lite sleep mode
    Signed-off-by: Weixin Zhou <zwx@rock-chips.com>
    Change-Id: Ia522f69c3edc939d00ea4aec87568d8df8af4a58

--- drivers/video/backlight/pwm_bl.c
+++ drivers/video/backlight/pwm_bl.c
@@ -671,6 +671,10 @@ static int pwm_backlight_suspend(struct device *dev)
 	struct backlight_device *bl = dev_get_drvdata(dev);
 	struct pwm_bl_data *pb = bl_get_data(bl);
 
+#ifdef CONFIG_ROCKCHIP_LITE_ULTRA_SUSPEND
+	if (mem_sleep_current == PM_SUSPEND_MEM_LITE)
+		return 0;
+#endif
 	if (pb->notify)
 		pb->notify(pb->dev, 0);
 
@@ -686,6 +690,10 @@ static int pwm_backlight_resume(struct device *dev)
 {
 	struct backlight_device *bl = dev_get_drvdata(dev);
 
+#ifdef CONFIG_ROCKCHIP_LITE_ULTRA_SUSPEND
+	if (mem_sleep_current == PM_SUSPEND_MEM_LITE)
+		return 0;
+#endif
 	backlight_update_status(bl);
 
 	return 0;
