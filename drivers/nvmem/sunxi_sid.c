9e95f4c3c3a6f42618626c57402b73d934e8a093
Kali Prasad <kprasadvnsi@protonmail.com>
2021-09-19 13:28:10 +0530

    drv:nvmem:sunxi_sid: Support SID on H616
    Add support for H616's SID controller. It supports 4K-bit
    EFUSE.
    
    Signed-off-by: Kali Prasad <kprasadvnsi@protonmail.com>

--- drivers/nvmem/sunxi_sid.c
+++ drivers/nvmem/sunxi_sid.c
@@ -241,6 +241,12 @@ static const struct sunxi_sid_cfg sun50i_h6_cfg = {
 	.size = 0x200,
 };
 
+static const struct sunxi_sid_cfg sun50i_h616_cfg = {
+	.value_offset = 0x200,
+	.size = 0x100,
+	.need_register_readout = true,
+};
+
 static const struct of_device_id sunxi_sid_of_match[] = {
 	{ .compatible = "allwinner,sun4i-a10-sid", .data = &sun4i_a10_cfg },
 	{ .compatible = "allwinner,sun7i-a20-sid", .data = &sun7i_a20_cfg },
@@ -250,6 +256,7 @@ static const struct of_device_id sunxi_sid_of_match[] = {
 	{ .compatible = "allwinner,sun50i-a64-sid", .data = &sun50i_a64_cfg },
 	{ .compatible = "allwinner,sun50i-h5-sid", .data = &sun50i_a64_cfg },
 	{ .compatible = "allwinner,sun50i-h6-sid", .data = &sun50i_h6_cfg },
+	{ .compatible = "allwinner,sun50i-h616-sid", .data = &sun50i_h616_cfg },
 	{/* sentinel */},
 };
 MODULE_DEVICE_TABLE(of, sunxi_sid_of_match);
