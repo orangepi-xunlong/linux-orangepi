ccfdb826e5edeb445fd186ccf2b1b76cb183f209
Tao Huang <huangtao@rock-chips.com>
2023-06-02 19:59:00 +0800

    net: wireless: rockchip_wlan: remove unused drivers
    Move to external driver repository.
    
    Change-Id: Iffef1967344b10749f6afa7b8b3948cf93cd0ec4
    Signed-off-by: Tao Huang <huangtao@rock-chips.com>

--- drivers/net/wireless/rockchip_wlan/rtl8188eu/include/rtw_version.h
+++
@@ -1,2 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#define DRIVERVERSION	"v5.7.6.1_36803.20200508"
--- drivers/net/wireless/rockchip_wlan/rtl8188fu/include/rtw_version.h
+++
@@ -1,2 +0,0 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-#define DRIVERVERSION	"v5.7.4.2_36687.20200421"
