ff23255bad85a6c5ceaabb85ba16205fa25b1fc8
baiywt <baiywt_gj@163.com>
2023-07-17 19:26:52 +0800

    scripts: Update gcc-wrapper.py

--- scripts/gcc-wrapper.py
+++ scripts/gcc-wrapper.py
@@ -102,7 +102,7 @@ def run_gcc():
         proc = subprocess.Popen(args, stderr=subprocess.PIPE, env=env)
         for line in proc.stderr:
             print (line.decode("utf-8"), end="")
-            interpret_warning(line.decode("utf-8"))
+            #interpret_warning(line.decode("utf-8"))
         if do_exit:
             sys.exit(1)
 
