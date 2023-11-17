c87b4d5c329c66a2fda5313f997e3c1c587d36d8
baiywt <baiywt_gj@163.com>
2023-11-17 17:04:53 +0800

    Update drivers/base/firmware_loader/main.c

--- drivers/base/firmware_loader/main.c
+++ drivers/base/firmware_loader/main.c
@@ -473,7 +473,8 @@ static const char * const fw_path[] = {
 	"/lib/firmware/updates/" UTS_RELEASE,
 	"/lib/firmware/updates",
 	"/lib/firmware/" UTS_RELEASE,
-	"/lib/firmware"
+	"/lib/firmware",
+	"/lib/firmware/ap6275p",
 };
 
 /*
