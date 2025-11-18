c0e7cd5997834aa3700d0a331f85156b42ae4ffc
ReleaseRaptor <ReleaseRaptor@cixtech.com>
2025-11-18 09:19:43 +0800

    ALSA: hda: realtek: disable auto mic by default
     - disable auto mic by default, since user may want to use
       dmic for recording even the headset plugin
    
    (cherry picked from commit be769c7b632b706bb0cc625ad42deca56a30d04c)

--- sound/pci/hda/patch_realtek.c
+++ sound/pci/hda/patch_realtek.c
@@ -7278,13 +7278,15 @@ static void alc_fixup_headset_mic(struct hda_codec *codec,
 	}
 }
 
-static void alc_fixup_3kpull_low(struct hda_codec *codec,
+static void alc_fixup_cix_phecda(struct hda_codec *codec,
 				 const struct hda_fixup *fix, int action)
 {
 	struct alc_spec *spec = codec->spec;
 
-	if (action == HDA_FIXUP_ACT_PRE_PROBE)
+	if (action == HDA_FIXUP_ACT_PRE_PROBE) {
 		spec->en_3kpull_low = false;
+		spec->gen.suppress_auto_mic = true;
+	}
 }
 
 enum {
@@ -7568,7 +7570,7 @@ enum {
 	ALC2XX_FIXUP_HEADSET_MIC,
 	ALC289_FIXUP_DELL_CS35L41_SPI_2,
 	ALC294_FIXUP_CS35L41_I2C_2,
-	ALC256_FIXUP_DIS_3KPULL_LOW,
+	ALC256_FIXUP_CIX_PHECDA,
 };
 
 /* A special fixup for Lenovo C940 and Yoga Duet 7;
@@ -9800,9 +9802,9 @@ static const struct hda_fixup alc269_fixups[] = {
 		.type = HDA_FIXUP_FUNC,
 		.v.func = cs35l41_fixup_i2c_two,
 	},
-	[ALC256_FIXUP_DIS_3KPULL_LOW] = {
+	[ALC256_FIXUP_CIX_PHECDA] = {
 		.type = HDA_FIXUP_FUNC,
-		.v.func = alc_fixup_3kpull_low,
+		.v.func = alc_fixup_cix_phecda,
 	},
 };
 
@@ -10308,7 +10310,7 @@ static const struct hda_quirk alc269_fixup_tbl[] = {
 	SND_PCI_QUIRK(0x10ec, 0x124c, "Intel Reference board", ALC295_FIXUP_CHROME_BOOK),
 	SND_PCI_QUIRK(0x10ec, 0x1252, "Intel Reference board", ALC295_FIXUP_CHROME_BOOK),
 	SND_PCI_QUIRK(0x10ec, 0x1254, "Intel Reference board", ALC295_FIXUP_CHROME_BOOK),
-	SND_PCI_QUIRK(0x10ec, 0x129e, "CIX Phecda Board", ALC256_FIXUP_DIS_3KPULL_LOW),
+	SND_PCI_QUIRK(0x10ec, 0x129e, "CIX Phecda Board", ALC256_FIXUP_CIX_PHECDA),
 	SND_PCI_QUIRK(0x10ec, 0x12cc, "Intel Reference board", ALC295_FIXUP_CHROME_BOOK),
 	SND_PCI_QUIRK(0x10f7, 0x8338, "Panasonic CF-SZ6", ALC269_FIXUP_ASPIRE_HEADSET_MIC),
 	SND_PCI_QUIRK(0x144d, 0xc109, "Samsung Ativ book 9 (NP900X3G)", ALC269_FIXUP_INV_DMIC),
